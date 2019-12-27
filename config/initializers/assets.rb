5abfee975c27ea673105ad31c29ab28bb766dcd1
Vinh <theten12@gmail.com>
2019-12-27 20:38:03 +0700

    Devise User's Name and Boostrap Clean-Blog template

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w( clean-blog.js )
+Rails.application.config.assets.precompile += %w( contact_me.js )
+Rails.application.config.assets.precompile += %w( jqBootstrapValidation.js )
