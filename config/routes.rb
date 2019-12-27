4086ed2ec27c040dc88090c11b602f56d44b13ea
Vinh <theten12@gmail.com>
2019-12-27 23:13:42 +0700

    Users and Posts seeder

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
-  resources :users, only: [:show]
   devise_for :users
   root 'pages#home'
   get 'pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+User.create!(name:  "Nguyen Vinh",
+  email: "theten12@gmail.com",
+  password:              "Theten12",
+  password_confirmation: "Theten12",
+  confirmed_at: Time.zone.now)
+
+99.times do |n|
+name  = Faker::Name.name
+email = "example-#{n+1}@railstutorial.org"
+password = "password"
+User.create!(name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    confirmed_at: Time.zone.now)
+end
+
+# Posts
+users = User.order(:created_at).take(6)
+50.times do
+title = Faker::Lorem.paragraph(sentence_count: 2)
+content = Faker::Lorem.paragraph(sentence_count: 20)
+users.each { |user| user.posts.create!(title: title, content: content) }
+end
