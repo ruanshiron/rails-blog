2e1eea28210725d446d3b2a1d0ee9a3281834f75
Vinh <theten12@gmail.com>
2019-12-27 15:16:55 +0700

    add Devise with Email Confirmation && Boostrap

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,9 +31,23 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000, protocol: 'http' }
+
+  config.action_mailer.delivery_method = :smtp
+  # host = 'lit-gorge-47261.herokuapp.com'
+  # config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.gmail.com',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => 'theten12@gmail.com',
+    :password       => 'thetenvinh',
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
